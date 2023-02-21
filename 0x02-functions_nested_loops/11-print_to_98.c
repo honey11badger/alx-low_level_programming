a0d1b435ebb36737c37b3825a5fff77cefbd7907
honey11badger <bisdemba@gmail.com>
2023-02-21 11:58:15 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,45 +1,23 @@
-#include "main.h"
-
 #include <stdio.h>
 
 /**
- * print_to_98 - prints 9 timetables starting with 0
- * @n: the number to start to print
- * Return: Always -
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
  */
-
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            if (n == 98)
-            {
-                printf("%d", n);
-                printf("\n");
-                break;
-            }
-            else
-            {
-                printf("%d, ", n);
-            }
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            if (n == 98)
-            {
-                printf("%d", n);
-                printf("\n");
-                break;
-            }
-            else
-            {
-                printf("%d, ", n);
-            }
-        }
-    }
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
